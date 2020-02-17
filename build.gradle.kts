f55098aa992269f92508e04287827de97ac6cfb2
Pierre Leresteux <pierre.leresteux@gmail.com>
2020-02-17 20:53:57 +0100

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,34 @@
+import Build_gradle.DependencyVersions.arrowVersion
+
+plugins {
+    kotlin("jvm") version "1.3.61"
+}
+
+group = "io.saagie"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    maven { setUrl("https://dl.bintray.com/kotlin/kotlin-eap") }
+    mavenCentral()
+}
+
+object DependencyVersions {
+    // Dependencies
+    const val arrowVersion = "0.10.4"
+    // TestsDependencies
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+    implementation("io.arrow-kt:arrow-core:$arrowVersion")
+    implementation("io.arrow-kt:arrow-syntax:$arrowVersion")
+}
+
+tasks {
+    compileKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+}
---
+++ src/main/kotlin/io/saagie/arrow/KotlinFP.kt
@@ -0,0 +1,20 @@
+package io.saagie.arrow
+
+fun printAll() {
+    println("5 x 1 = 5")
+    println("5 x 2 = 10")
+    println("5 x 3 = 15")
+    println("5 x 4 = 20")
+    println("5 x 5 = 25")
+    println("5 x 6 = 30")
+    println("5 x 7 = 35")
+    println("5 x 8 = 40")
+    println("5 x 9 = 45")
+    println("5 x 10 = 50")
+}
+
+
+fun main() {
+    printAll()
+}
+
